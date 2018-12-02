678e07755fe9fec66ecb48307a21e777dc947ac4
Fahim Mannan <fahim.mannan@algolux.com>
2018-12-01 18:30:18 -0800

    added display_env cmdline option for visdom

--- cyclegan/scripts/train_cyclegan_dss_axdata.sh
+++ cyclegan/scripts/train_cyclegan_dss_axdata.sh
@@ -1,2 +1,3 @@
 set -ex
-python train.py --dataroot /mnt/data2/dataset/cycada_data/dss_axdata/ --name dss_axdata --model cycle_gan --pool_size 50 --no_dropout
+python train.py --dataroot /data6/cycada_data/dss_axdata --name dss_axdata2 --model cycle_gan --pool_size 50 --no_dropout --display_port 6006 --checkpoints_dir=/data6/cycada_data/ckpts/
+
